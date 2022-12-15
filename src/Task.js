22e679c0a7c69338b7cf49015d1e2a553f1e7589
Randika <randikaa1999@gmail.com>
2022-12-15 23:32:11 +0700

    Update file src/Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://randika-fe2835657.netlify.app/"; // TODO: replace this
 const studentName = "Randika"; // TODO: replace this
 const studentId = "FE2835657"; // TODO: replace this
 
